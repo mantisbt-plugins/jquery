a37f75c62ec13e04c73c3c4b9fea8dd8a9a69941
John Reese <jreese@leetcode.net>
2010-04-08 23:32:45 -0400

    Fix conflict with Projax/Prototype used by Mantis
    There was a conflict with the Projax/Prototype libraries included with
    MantisBT over the usage of the $() construct.  To solve this, jQuery
    will only listen on $() inside of jQuery(document).ready().
    
    This means that any plugins using $() outside of .ready() will need to
    update their scripts to follow the new expectations.

--- jQuery/jQuery.php
+++ jQuery/jQuery.php
@@ -41,7 +41,8 @@ class jQueryPlugin extends MantisPlugin {
 	 * Create the resource link to load the jQuery library.
 	 */
 	function resources( $p_event ) {
-		return '<script type="text/javascript" src="' . plugin_file( 'jquery-min.js' ) . '"></script>';
+		return '<script type="text/javascript" src="' . plugin_file( 'jquery-min.js' ) . '"></script>'.
+			'<script type="text/javascript">jQuery.noConflict();</script>';
 	}
 }
 
